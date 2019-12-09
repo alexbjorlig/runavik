575a9f103bda5552722a45de9edef09c29aca5c3
Alex Bjørlig <alexbjorlig@icloud.com>
2019-12-09 13:23:52 +0100

    added switch

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { rotaryEncoder } from "./rotary-encoder";
 import { cleanupLcd, printToLCD } from "./lcd";
+const Gpio = require("onoff").Gpio;
+
+const button3 = new Gpio(7, "in", "both", { debounceTimeout: 10 });
+
+console.log(button3.readSync());
+
+button3.watch(function(err, value) {
+  if (err) {
+    throw err;
+  }
+  console.log(`Button3 ${value}`);
+});
 
 console.log("Starting up!");
 
